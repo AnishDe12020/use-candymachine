a71c6ce275f84b80af12ac4610e77de82e4adb33
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2022-09-20 14:46:37 +0530

    feat(use-candymachine): :sparkles: new `changePage` function and add total number of pages to metadata

--- src/use-candymachine.ts
+++ src/use-candymachine.ts
@@ -2,110 +2,128 @@ import { Connection, PublicKey } from "@solana/web3.js";
 import { CandyMachine, Metaplex } from "@metaplex-foundation/js";
 import { useMemo, useState } from "react";
 
+interface CandyMachineMetaWithPages extends CandyMachine {
+  totalPages: number;
+}
+
 const useCandymachine = (
-	conn: Connection,
-	cmId: string,
-	nftsPerPage: number
+  conn: Connection,
+  cmId: string,
+  nftsPerPage: number
 ) => {
-	const [page, setPage] = useState<number>(1);
-	const [candymachineMeta, setCandymachineMeta] = useState<CandyMachine | null>(
-		null
-	);
-	const [nfts, setNfts] = useState<any[] | null>(null);
+  const [page, setPage] = useState<number>(1);
+  const [candymachineMeta, setCandymachineMeta] =
+    useState<CandyMachineMetaWithPages | null>(null);
+  const [nfts, setNfts] = useState<any[] | null>(null);
 
-	const metaplex = useMemo(() => {
-		return Metaplex.make(conn);
-	}, [conn]);
+  const metaplex = useMemo(() => {
+    return Metaplex.make(conn);
+  }, [conn]);
 
-	const prevPage = async () => {
-		if (page - 1 < 1) {
-			setPage(1);
-		} else {
-			setPage(page - 1);
-		}
-		await fetchNfts();
-	};
+  const prevPage = async () => {
+    if (page - 1 < 1) {
+      setPage(1);
+    } else {
+      setPage(page - 1);
+    }
+    await fetchNfts();
+  };
 
-	const nextPage = async () => {
-		setPage(page + 1);
-		await fetchNfts();
-	};
+  const nextPage = async () => {
+    setPage(page + 1);
+    await fetchNfts();
+  };
 
-	const fetchCandyMachineMetadata = async () => {
-		if (!cmId) {
-			console.error("No candy machine id provided");
-			return;
-		}
+  const fetchCandyMachineMetadata = async () => {
+    if (!cmId) {
+      console.error("No candy machine id provided");
+      return;
+    }
 
-		try {
-			const candymachine = await metaplex
-				.candyMachines()
-				.findByAddress({ address: new PublicKey(cmId) })
-				.run();
+    try {
+      const candymachine = await metaplex
+        .candyMachines()
+        .findByAddress({ address: new PublicKey(cmId) })
+        .run();
 
-			setCandymachineMeta(candymachine);
+      const totalPages = Math.ceil(candymachine.itemsAvailable / nftsPerPage);
 
-			return candymachine;
-		} catch (error) {
-			console.error(error);
-		}
-	};
+      setCandymachineMeta({ ...candymachine, totalPages });
 
-	const initialFetch = async () => {
-		const meta = await fetchCandyMachineMetadata();
-		console.log("candymachineMeta fetched");
-		console.log("f", meta);
-		await fetchNfts(meta);
-	};
+      return candymachine;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const fetchNftsForPage = async (page: number, meta: CandyMachine) => {
-		if (!meta) {
-			console.error(
-				"No candy machine metadata found. Please run `fetchCandyMachine` first"
-			);
-			return;
-		}
+  const initialFetch = async () => {
+    const meta = await fetchCandyMachineMetadata();
+    console.log("candymachineMeta fetched");
+    console.log("f", meta);
+    await fetchNfts(meta);
+  };
 
-		const pageItems = meta.items.slice(
-			(page - 1) * nftsPerPage,
-			page * nftsPerPage
-		);
+  const fetchNftsForPage = async (page: number, meta: CandyMachine) => {
+    if (!meta) {
+      console.error(
+        "No candy machine metadata found. Please run `fetchCandyMachine` first"
+      );
+      return;
+    }
 
-		let nftData: any[] = [];
+    const pageItems = meta.items.slice(
+      (page - 1) * nftsPerPage,
+      page * nftsPerPage
+    );
 
-		for (let i = 0; i < pageItems.length; i++) {
-			const fetchResult = await fetch(pageItems[i].uri);
-			const jsonResult = await fetchResult.json();
-			nftData.push(jsonResult);
-		}
+    let nftData: any[] = [];
 
-		setNfts(nftData);
-	};
+    for (let i = 0; i < pageItems.length; i++) {
+      const fetchResult = await fetch(pageItems[i].uri);
+      const jsonResult = await fetchResult.json();
+      nftData.push(jsonResult);
+    }
 
-	const fetchNfts = async (meta?: CandyMachine) => {
-		if (!meta) {
-			if (candymachineMeta) {
-				meta = candymachineMeta;
-			} else {
-				console.error(
-					"No candy machine metadata found. Please run `fetchCandyMachine` first"
-				);
-				return;
-			}
-		}
-		await fetchNftsForPage(page, meta);
-	};
+    setNfts(nftData);
+  };
 
-	return {
-		page,
-		candymachineMeta,
-		nfts,
-		prevPage,
-		nextPage,
-		fetchCandyMachineMetadata,
-		initialFetch,
-		fetchNftsForPage,
-	};
+  const fetchNfts = async (meta?: CandyMachine) => {
+    if (!meta) {
+      if (candymachineMeta) {
+        meta = candymachineMeta;
+      } else {
+        console.error(
+          "No candy machine metadata found. Please run `fetchCandyMachine` first"
+        );
+        return;
+      }
+    }
+    await fetchNftsForPage(page, meta);
+  };
+
+  const changePage = async (page: number) => {
+    if (!candymachineMeta) {
+      console.error(
+        "No candy machine metadata found. Please run `fetchCandyMachine` first"
+      );
+      return;
+    }
+    await fetchNftsForPage(page, candymachineMeta);
+    setPage(page);
+    console.log("changed page");
+  };
+
+  return {
+    page,
+    candymachineMeta,
+    nfts,
+    prevPage,
+    nextPage,
+    fetchCandyMachineMetadata,
+    initialFetch,
+    fetchNftsForPage,
+    changePage,
+  };
 };
 
 export default useCandymachine;
